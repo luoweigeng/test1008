ae008a025a6f29152072d7bff071886418369eb0
luoweigeng <54936525+luoweigeng@users.noreply.github.com>
2019-10-08 11:10:33 +0800

    Add files via upload

--- Demo1_2.java
+++ Demo1_2.java
@@ -1,9 +1,11 @@
-/*
+﻿/*
  * 2019/3/23 21:12
  */
 public class Demo1_2 {
 public static void main(String[] args){
 	System.out.println("hello,GitHub！");
 	System.out.println("Chenhuilin,I love U");
+	System.out.println("陈慧琳,I love U");
+	
 }
 }
